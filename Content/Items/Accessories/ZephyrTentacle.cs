6de3604bfa4446dd612ffb20f216393ffb70a192
magegor11 <magegor11@gmail.com>
2025-07-09 07:33:14 +0300

    Add files via upload

---
+++ Content/Items/Accessories/ZephyrTentacle.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Terraria;
+using Terraria.ModLoader;
+
+namespace UltraniumUnearthed.Content.Items.Accessories;
+
+public class ZephyrTentacle : ModItem
+{
+  
+
+  public override void SetDefaults()
+  {
+    Item.width = 32;
+    Item.height = 32;
+    Item.rare = 4;
+    Item.value = 200000;
+    Item.accessory = true;
+    Item.expert = true;
+  }
+
+  public override void UpdateAccessory(Player player, bool hideVisual)
+  {
+        player.moveSpeed += 0.10f;
+        player.jumpSpeedBoost += 4f;
+    }
+}
